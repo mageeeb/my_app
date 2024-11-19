0de8f1eab62f06f72160203d085f6f687fcc7ff5
Baye Fall <magibsall@gmail.com>
2024-11-19 11:17:47 +0100

    sov2

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+class Etudiant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateNaissance = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $genre = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $classe = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $note = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $dateNaissance): static
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): static
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getClasse(): ?string
+    {
+        return $this->classe;
+    }
+
+    public function setClasse(string $classe): static
+    {
+        $this->classe = $classe;
+
+        return $this;
+    }
+
+    public function getNote(): array
+    {
+        return $this->note;
+    }
+
+    public function setNote(array $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+    // Calculer la moyenne générale
+    public function getMoyenneGenerale(): ?float
+    {
+        if (empty($this->notes)) {
+            return null;
+        }
+
+        $somme = array_sum($this->notes);
+        $nombreDeNotes = count($this->notes);
+
+        return $nombreDeNotes > 0 ? round($somme / $nombreDeNotes, 2) : null;
+    }
+
+}
---
+++ src/Form/EtudiantType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etudiant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtudiantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNaissance', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('genre', ChoiceType::class, [
+                'choices' => [
+                    'Masculin' => 'M',
+                    'Féminin' => 'F',
+                ],
+            ])
+            ->add('classe', ChoiceType::class, [
+                'choices' => [
+                    'Classe 1' => 'classe_1',
+                    'Classe 2' => 'classe_2',
+                ],
+            ])
+            ->add('photo', FileType::class, [
+                'required' => false,
+                'mapped' => false, // Ce champ n'est pas lié à l'entité
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etudiant::class,
+        ]);
+    }
+}
